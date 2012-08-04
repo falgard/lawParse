ebc51abc478865526a05ec0b010ae51093e95077
Mikael <sup3rgnu@gmail.com>
2012-08-04 13:52:07 +0200

    test commit

--- SFS.py
+++ SFS.py
@@ -57,6 +57,8 @@ class SFSController(Source.Controller):
 	# Actual parsing begins here.
 	#p = SFSParser()
 
+	#test 
+
 	def ParseAll(self):
 		dlDir = os.path.sep.join([self.baseDir, u'sfs', 'dl', 'sfst'])
 		self._runMethod(dlDir, 'html', self.Parse)
